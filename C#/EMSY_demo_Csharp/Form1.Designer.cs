db1241ddaa103d0c33bfde713e44b633dab0ac4c
Swiss_Maverick <144990041+SwissMaverick@users.noreply.github.com>
2026-02-02 08:37:38 +0100

    Ajout code démo

---
+++ C#/EMSY_demo_Csharp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace EMSY_demo_Csharp
+{
+    partial class Ligma
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Yaris = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // Yaris
+            // 
+            this.Yaris.Location = new System.Drawing.Point(361, 122);
+            this.Yaris.Name = "Yaris";
+            this.Yaris.Size = new System.Drawing.Size(75, 23);
+            this.Yaris.TabIndex = 0;
+            this.Yaris.Text = "Toyota";
+            this.Yaris.UseVisualStyleBackColor = true;
+            this.Yaris.Click += new System.EventHandler(this.Balls_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(438, 207);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(206, 227);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // Ligma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Yaris);
+            this.Name = "Ligma";
+            this.Text = "Ligma";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button Yaris;
+        private CheckBox checkBox1;
+        private CheckedListBox checkedListBox1;
+    }
+}
---
+++ C#/EMSY_demo_Csharp/Form1.cs
@@ -0,0 +1,12 @@
+namespace EMSY_demo_Csharp
+{
+    public partial class Ligma : Form
+    {
+        public Ligma()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
