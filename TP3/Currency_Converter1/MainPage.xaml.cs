4ff2ad07da0d82955cc3bc5e80a11881ce152f48
Swiss_Maverick <144990041+SwissMaverick@users.noreply.github.com>
2026-03-02 14:49:08 +0100

    shmol update

--- TP3/Currency_Converter1/MainPage.xaml.cs
+++ TP3/Currency_Converter1/MainPage.xaml.cs
@@ -22,9 +22,91 @@ namespace Currency_Converter1
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        double TauxChange = 0;
+        double Resultat;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void rbnSFR_Checked(object sender, RoutedEventArgs e)
+        {
+            if (rbnSFR.Checked)
+            {
+                TauxChange = 0.90665;
+
+                PicDevise.Image = imageList1.Images[0];
+            }
+        }
+
+        private void rbnUSD_Checked(object sender, RoutedEventArgs e)
+        {
+            if (rbnUSD.Checked)
+            {
+                TauxChange = 0.92;
+
+                PicDevise.Image = imageList1.Images[1];
+            }
+        }
+
+        private void rbnGBP_Checked(object sender, RoutedEventArgs e)
+        {
+            if (rbnGBP.Checked)
+            {
+                TauxChange = 1.17;
+
+                PicDevise.Image = imageList1.Images[2];
+            }
+        }
+
+        private void rbnYEN_Checked(object sender, RoutedEventArgs e)
+        {
+            if (rbnYEN.Checked)
+            {
+                TauxChange = 0.006;
+
+                PicDevise.Image = imageList1.Images[3];
+            }
+        }
+
+        private void rbnYUEN_Checked(object sender, RoutedEventArgs e)
+        {
+            if (rbnYUEN.Checked)
+            {
+                TauxChange = 0.12;
+
+                PicDevise.Image = imageList1.Images[4];
+            }
+        }
+
+        private void rbnAUD_Checked(object sender, RoutedEventArgs e)
+        {
+            TauxChange = 0.59;
+
+            PicDevise.Image = imageList1.Images[5];
+        }
+
+        private void btnConvertir_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (TauxChange == 0)
+                {
+                    MessageBox.Show("Veuillez sélectionner une devise.");
+                    return;
+                }
+
+                double Montant = double.Parse(txtMontant.Text);
+
+                double Resultat = Montant * TauxChange;
+
+                txtResultat.Text = Resultat.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Erreur : Veuillez entrer un montant numérique valide (ex: 100).");
+            }
         }
     }
 }
